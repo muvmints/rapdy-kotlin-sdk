196607410688d11029ba5b475f3152cbabcc4020
justin emina <justin.emina@ideazlab.co.uk>
2025-11-22 17:48:50 +0000

    **Enhanced build configuration and publishing setup**
    - Switched fixed plugin and dependency versions to dynamic system properties for flexibility.
    - Added repositories for Maven local, releases, and snapshots with secure credentials.
    - Configured Maven `publishing` for artifact deployment with metadata (SCM, developer info, and licensing).
    - Updated `kotlinVersion` to 2.2.21 and synchronized related properties.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,46 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.9.25"
-    id("org.jetbrains.kotlin.plugin.allopen") version "1.9.25"
-    id("com.google.devtools.ksp") version "1.9.25-1.0.20"
-    id("io.micronaut.library") version "4.6.1"
-    id("com.gradleup.shadow") version "8.3.9"
-    id("io.micronaut.aot") version "4.6.1"
+    val kotlinVersion: String by System.getProperties()
+    val ksp: String by System.getProperties()
+    val micronautPluginVersion: String by System.getProperties()
+    id("org.jetbrains.kotlin.jvm") version kotlinVersion
+    id("org.jetbrains.kotlin.plugin.allopen") version kotlinVersion
+    id("com.google.devtools.ksp") version ksp
+    id("io.micronaut.library") version micronautPluginVersion
+    id("io.micronaut.aot") version micronautPluginVersion
+    `maven-publish`
 }
 
-version = "0.1"
-group = "com.ideazlab.jeie.muvmints.rapyd"
-
-val kotlinVersion=project.properties.get("kotlinVersion")
+val mavenSecret: String by System.getProperties()
+val kotlinVersion = project.properties.get("kotlinVersion")
+val appVersion: String by System.getProperties()
+val group: String by System.getProperties()
 repositories {
+    mavenLocal()
     mavenCentral()
+    maven {
+        name = "reposiliteRepositoryReleases"
+        url = uri("https://maven.ideazlab.co.uk/releases")
+        isAllowInsecureProtocol = true
+        credentials {
+            username = "gradle"
+            password = mavenSecret
+        }
+        authentication {
+            create<BasicAuthentication>("basic")
+        }
+    }
+    maven {
+        name = "reposiliteRepositorySnapshots"
+        url = uri("https://maven.ideazlab.co.uk/snapshots")
+        isAllowInsecureProtocol = true
+        credentials {
+            username = "admin"
+            password = mavenSecret
+        }
+        authentication {
+            create<BasicAuthentication>("basic")
+        }
+    }
 }
 
 dependencies {
@@ -45,7 +73,6 @@ java {
 graalvmNative.toolchainDetection = false
 
 micronaut {
-    runtime("netty")
     testRuntime("kotest5")
     processing {
         incremental(true)
@@ -65,5 +92,51 @@ micronaut {
     }
 }
 
-
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            pom {
+                name = "MuvMints Rapdy SDK"
+                description = "SDK for Rapyd payment platform API"
+                url = "https://www.ideazlab.co.uk"
+                licenses {
+                    license {
+                        name = "The Apache License, Version 2.0"
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "justinemina"
+                        name = "Justin Emina"
+                        email = "Justin.Justin@ideazlab.co.uk"
+                    }
+                }
+                scm {
+                    connection = "scm:git@github.com:muvmints/rapdy-kotlin-sdk.git"
+                    developerConnection = "scm:git@github.com:muvmints/rapdy-kotlin-sdk.git"
+                    url = "https://www.ideazlab.co.uk"
+                }
+            }
+            groupId = group
+            artifactId = "rapdy-kotlin-sdk"
+            version = appVersion
+            repositories {
+                maven {
+                    val releasesRepoUrl = "https://maven.ideazlab.co.uk/releases"
+                    val snapshotsRepoUrl = "https://maven.ideazlab.co.uk/snapshots"
+                    url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+                    credentials {
+                        username = "admin"
+                        password = mavenSecret
+                    }
+                    authentication {
+                        create<BasicAuthentication>("basic")
+                    }
+                }
+            }
+            from(components["java"])
+        }
+    }
+}
 
